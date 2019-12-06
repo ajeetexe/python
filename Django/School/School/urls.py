776c1118bb3542279ff8df987943a394eac78e17
ajeetexe <46785184+ajeetexe@users.noreply.github.com>
2019-12-06 20:54:28 +0530

    files commited

--- Django/School/School/urls.py
+++ Django/School/School/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from Management import views
 urlpatterns = [
+    path('',views.Index.as_view(),name='home'),
     path('admin/', admin.site.urls),
+    path('student/',include('Student.urls')),
+    path('teacher/',include('Teacher.urls')),
+    path('management/',include('Management.urls')),
 ]
--- Django/School/Student/models.py
+++ Django/School/Student/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+class RegisterStudent(models.Model):
+    student_id = models.AutoField(primary_key=True,unique=True) 
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    dob = models.DateField()
+    address = models.CharField(max_length=100)
+    standard = models.CharField(max_length=5)
+
+
+    def __str__(self):
+        return self.first_name + self.last_name
+        print(self.student_id)
+
+    def get_absolute_url(self):
+        return reverse("student_home")
