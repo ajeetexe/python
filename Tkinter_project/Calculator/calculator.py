86420d85e9fa09f5371613475e0d6e2c2cafd36c
ajeetexe <46785184+ajeetexe@users.noreply.github.com>
2020-01-14 11:43:28 +0530

    all commited

---
+++ Tkinter_project/Calculator/calculator.py
@@ -0,0 +1,15 @@
+from tkinter import Tk, Button, Label, Frame
+
+
+class Calc:
+    def __init__(self, master):
+        master.title("hello")
+
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    Calc(root)
+    root.mainloop()
